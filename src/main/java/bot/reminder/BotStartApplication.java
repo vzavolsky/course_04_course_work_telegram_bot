04a89b2df224d8a5e56515fc61a362f3685d2ac7
vzavolsky <vzavolsky@ya.ru>
2023-06-10 23:02:34 +0300

    Course 04 - Course work - Telegram Bot "Reminder". Initial commit.

---
+++ src/main/java/bot/reminder/BotStartApplication.java
@@ -0,0 +1,13 @@
+package bot.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bot/reminder/BotStartApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
